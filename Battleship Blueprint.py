89df6456f0d7a8fc760d201aab26e6b580ca8eac
Miller-Wood <138690523+Miller-Wood@users.noreply.github.com>
2023-07-17 08:57:35 -0400

    Add files via upload

--- Battleship Blueprint.py
+++ Battleship Blueprint.py
@@ -23,50 +23,42 @@ def create(board):
      choose_col = randrange(0, len(board[0]))
      choose_row = randrange(0, len(board[0]))
     
-     create_ship = choose_col, choose_row
-    
      h_v = randrange(0,2)
      if h_v == 0:
         if choose_col == len(board[0])-1:
              returnable.append([choose_col - 2, choose_row])
              returnable.append([choose_col - 1, choose_row])
-             print(returnable[1])
-             returnable.append([create_ship])
+             returnable.append([choose_col, choose_row])
         elif choose_col == 0:
-             returnable.append([create_ship])
+             returnable.append([choose_col, choose_row])
              returnable.append([choose_col + 1, choose_row])
-             print(returnable[1])
              returnable.append([choose_col + 2, choose_row])
         else:
              returnable.append([choose_col + 1, choose_row])
-             returnable.append([create_ship])
-             print(returnable[1])
+             returnable.append([choose_col, choose_row])
              returnable.append([choose_col - 1, choose_row])
      elif h_v == 1:
          if choose_row == len(board[0])-1:
              returnable.append([choose_col, choose_row - 2])
              returnable.append([choose_col, choose_row - 1])
-             print(returnable[1])
-             returnable.append([create_ship])
+             returnable.append([choose_col, choose_row])
          elif choose_row == 0:
-             returnable.append([create_ship])
+             returnable.append([choose_col, choose_row])
              returnable.append([choose_col, choose_row + 1])
-             print(returnable[1])
              returnable.append([choose_col, choose_row + 2])
          else:
              returnable.append([choose_col, choose_row + 1])
-             returnable.append([create_ship])
-             print(returnable[1])
+             returnable.append([choose_col, choose_row])
              returnable.append([choose_col, choose_row - 1])
      return returnable
 #Creates a Reference-Board that has the ships
 def MakeReferenceBoard(board, coords):
-    # a = int(coords[0][0])
-    # b = int(coords[0][1])
-    # c = int(coords[1][0])
-    # d = int(coords[1][1])
-    # e = int(coords[2][0])
-    # f = int(coords[2][1])
+    a = int(coords[0][0])
+    b = int(coords[0][1])
+    c = int(coords[1][0])
+    d = int(coords[1][1])
+    e = int(coords[2][0])
+    f = int(coords[2][1])
     # print(coords[0])
     # print(coords[1])
     # print(coords[2])
@@ -77,7 +69,7 @@ def MakeReferenceBoard(board, coords):
     newBoard[e][f] = "[ X ]"
     return newBoard
 
-def play(board):
+def play(board, returnable):
     row_list = []
     column_list = []
     print("Welcome to BATTLESHIP! My battleship is placed randomly on the board. You will get 5 chances to sink my ship. Colomns and rows are labled 0-5. Choose wisely and have fun!")
@@ -98,6 +90,14 @@ def play(board):
             print("Numbers only please")
         else:       
             print("Missile Launch")
+        if (returnable[0] == row_list[len(row_list)-1], column_list[len(row_list)-1]) or (returnable[1] == row_list[len(row_list)-1], column_list[len(row_list)-1]) or (returnable[2] == row_list[len(row_list)-1], column_list[len(row_list)-1]):
+            print("HIT!")
+            PrintBoard(board)
+            print("Just pretend we put an ! where you just hit lmao")
+        else:
+            print("MISS!")
+            PrintBoard(board)
+            print("Just pretend we put an O where you just missed lmao")
         turn = turn + 1
     while turn >= 5:
         print("You have run out of turns.")
@@ -106,7 +106,7 @@ def play(board):
 board = MakeBoard(10)
 #PrintBoard(board)
 coords = create(board)
-#OtherBoard = MakeReferenceBoard(board, coords)
-print(coords)
+OtherBoard = MakeReferenceBoard(board, coords)
+PrintBoard(OtherBoard)
 #print(coords[0][1])
-#play(board)
+#play(board, coords)
